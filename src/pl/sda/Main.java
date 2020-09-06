2eb0bb00ffb80b4e5b37f32e8fdfa3a332640dcc
KamilWyrwinski <kamil.wyrwinski@onet.pl>
2020-09-06 15:48:23 +0200

    następne zmiany

--- src/pl/sda/Main.java
+++ src/pl/sda/Main.java
@@ -3,6 +3,7 @@ package pl.sda;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        
+        System.out.println("second line");
+
     }
 }
